27a3e44444e330e32b63b2efa6fbb123d1e7d37a
Sibirny Nikita <rxstr1ker007@gmail.com>
2021-01-28 16:27:24 +0300

    test

--- src/main/java/tech/visdom/b2school_server/B2schoolServerApplication.java
+++ src/main/java/tech/visdom/b2school_server/B2schoolServerApplication.java
@@ -22,7 +22,7 @@ public class B2schoolServerApplication extends SpringBootServletInitializer {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
-    //Test112
+    //Test1122
     public static void main(String[] args) {
         SpringApplication.run(B2schoolServerApplication.class, args);
     }
